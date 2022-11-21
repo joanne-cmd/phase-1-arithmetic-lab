e81f8e0d7afc8025ce7a2ee800bf520a98617d9e
joanne muthoni <joanne.mumbi@student.moringaschool.com>
2022-11-21 19:02:48 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
-
 // Write your code here
+let num1,num2,num3,num4;
+num1=31;
+num2=2;
+let multiply= num1*num2;
+let random;
+random=Math.ceil( (Math.random()+10));
+let mod;
+num4=24;
+num3=10;
+mod=num4%num3;
+let max;
+max=Math.max(9,11,12,15,20)
+
